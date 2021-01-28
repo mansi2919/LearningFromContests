d6020f8ab86876c1d7795f4c4dfd9f623b9e6128
mansi2919 <41909232+mansi2919@users.noreply.github.com>
2021-01-28 18:02:22 +0530

    Update and rename Mai_Hu_JIyaan.cpp to Mai_Hu_Jiyaan.cpp

--- Mai_Hu_Jiyaan.cpp
+++ Mai_Hu_Jiyaan.cpp
@@ -9,7 +9,10 @@ int main(){
     while(cin>>temp){
         vs.push_back(temp);
     }
+  // the vector vs will store the words separately.
     for(auto it = vs.begin();it!=vs.end();it++)
         cout<<*it<<endl;
     return 0;
 }
+
+// Note - Global Array can have size upto 10^7 ( Plan for Nothing Hackerearth)
